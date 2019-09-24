6332f4229b42b46cc68d5febe23715e0196e3e25
jeffreytang97 <jeffreytang97@gmail.com>
2019-09-23 21:08:32 -0400

    Python file created.

---
+++ X_Rudder_Game.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+Comp 472 - X-Rudder Game Development
+Python Script
+Date Created: 2019-09-23
+
+Author:
+    - Jeffrey Tang
+    - Hong Yi Wang
+    - Ethan Tran
+"""
+
+"""
+PS: We should create a main function to run all the functions required instead of calling each function one by one.
+
+"""
+# Just an example
+def test_example():
+    print("Welcome to X-Rudder game!")
+
+def run_Game_Main_Function():
+    # This function will contain all the other functions that needs to be run.
+    test_example()
+    
+
+run_Game_Main_Function()
