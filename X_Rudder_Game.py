9a8bb5d798725eb078240bd018a24f67166820e5
Ea10 <ethanntrann@gmail.com>
2019-09-29 16:35:13 -0400

    change board for easier tile insertion

--- X_Rudder_Game.py
+++ X_Rudder_Game.py
@@ -24,6 +24,7 @@ def run_Game_Main_Function():
     # This function will contain all the other functions that needs to be run.
     test_example()
     Board = BoardClass()
+    Board.create_board()
     Board.print_board()
     #entry_selection()
     Board.addCoordinate("A1", "X")
@@ -34,175 +35,177 @@ class BoardClass:
         print("This is the board")
     
         # function to print/reset(?) the board
-    def print_board(self):
-        print("\nideal board:\n")
-        print("10|_|_|_|_|_|_|_|_|_|_|_|_|")
-        print(" 9|_|_|_|_|_|_|_|_|_|_|_|_|")
-        print(" 8|_|_|_|_|_|_|_|_|_|_|_|_|")
-        print(" 7|_|_|_|_|x|_|_|_|_|_|_|_|")
-        print(" 6|_|_|_|_|_|o|x|_|_|_|_|_|")
-        print(" 5|_|_|_|_|_|_|_|o|_|_|_|_|")
-        print(" 4|_|_|_|_|_|_|_|_|_|_|_|_|")
-        print(" 3|_|_|_|_|_|_|_|_|_|_|_|_|")
-        print(" 2|_|_|_|_|_|_|_|_|_|_|_|_|")
-        print(" 1|_|_|_|_|_|_|_|_|_|_|_|_|")
-        print("   A B C D E F G H I J K L")
-    
-        print("")
-    
-        # printing board using double list - access to elements will be slower than dictionary
-        double_list = [["A!", "B!", "C!"], ["A9", "B9", "C9"], ["A8", "B8", "C8"]]
-        for xs in double_list:
-            print("|".join(xs))
-    
-        print("")
+    def create_board(self):
+        # print("\nideal board:\n")
+        # print("10|_|_|_|_|_|_|_|_|_|_|_|_|")
+        # print(" 9|_|_|_|_|_|_|_|_|_|_|_|_|")
+        # print(" 8|_|_|_|_|_|_|_|_|_|_|_|_|")
+        # print(" 7|_|_|_|_|x|_|_|_|_|_|_|_|")
+        # print(" 6|_|_|_|_|_|o|x|_|_|_|_|_|")
+        # print(" 5|_|_|_|_|_|_|_|o|_|_|_|_|")
+        # print(" 4|_|_|_|_|_|_|_|_|_|_|_|_|")
+        # print(" 3|_|_|_|_|_|_|_|_|_|_|_|_|")
+        # print(" 2|_|_|_|_|_|_|_|_|_|_|_|_|")
+        # print(" 1|_|_|_|_|_|_|_|_|_|_|_|_|")
+        # print("   A B C D E F G H I J K L")
+        #
+        # print("")
+        #
+        # # printing board using double list - access to elements will be slower than dictionary
+        # double_list = [["A!", "B!", "C!"], ["A9", "B9", "C9"], ["A8", "B8", "C8"]]
+        # for xs in double_list:
+        #     print("|".join(xs))
+        #
+        # print("")
     
         # proof of concept to show all the labelled tiles we'd have if implementing with dictionary
         # since a dictionary is used, modification of tiles when a player makes a move will be much
         # easier
         # for rule, checking, can check if certain keys have the same value for win condition?
         # all key, value pairs are manually defined... there has to be a better way to do this
-        self.dictionary_poc = {
-            "A10": "A!",
-            "B10": "B!",
-            "C10": "C!",
-            "D10": "D!",
-            "E10": "E!",
-            "F10": "F!",
-            "G10": "G!",
-            "H10": "H!",
-            "I10": "I!",
-            "J10": "J!",
-            "K10": "K!",
-            "L10": "L!",
-            "A9": "A9",
-            "B9": "B9",
-            "C9": "C9",
-            "D9": "D9",
-            "E9": "E9",
-            "F9": "F9",
-            "G9": "G9",
-            "H9": "H9",
-            "I9": "I9",
-            "J9": "J9",
-            "K9": "K9",
-            "L9": "L9",
-    
-            "A8": "A8",
-            "B8": "B8",
-            "C8": "C8",
-            "D8": "D8",
-            "E8": "E8",
-            "F8": "F8",
-            "G8": "G8",
-            "H8": "H8",
-            "I8": "I8",
-            "J8": "J8",
-            "K8": "K8",
-            "L8": "L8",
-            "A7": "A7",
-            "B7": "B7",
-            "C7": "C7",
-            "D7": "D7",
-            "E7": "E7",
-            "F7": "F7",
-            "G7": "G7",
-            "H7": "H7",
-            "I7": "I7",
-            "J7": "J7",
-            "K7": "K7",
-            "L7": "L7",
-    
-            "A6": "A6",
-            "B6": "B6",
-            "C6": "C6",
-            "D6": "D6",
-            "E6": "E6",
-            "F6": "F6",
-            "G6": "G6",
-            "H6": "H6",
-            "I6": "I6",
-            "J6": "J6",
-            "K6": "K6",
-            "L6": "L6",
-            "A5": "A5",
-            "B5": "B5",
-            "C5": "C5",
-            "D5": "D5",
-            "E5": "E5",
-            "F5": "F5",
-            "G5": "G5",
-            "H5": "H5",
-            "I5": "I5",
-            "J5": "J5",
-            "K5": "K5",
-            "L5": "L5",
-    
-            "A4": "A4",
-            "B4": "B4",
-            "C4": "C4",
-            "D4": "D4",
-            "E4": "E4",
-            "F4": "F4",
-            "G4": "G4",
-            "H4": "H4",
-            "I4": "I4",
-            "J4": "J4",
-            "K4": "K4",
-            "L4": "L4",
-            "A3": "A3",
-            "B3": "B3",
-            "C3": "C3",
-            "D3": "D3",
-            "E3": "E3",
-            "F3": "F3",
-            "G3": "G3",
-            "H3": "H3",
-            "I3": "I3",
-            "J3": "J3",
-            "K3": "K3",
-            "L3": "L3",
-    
-            "A2": "A2",
-            "B2": "B2",
-            "C2": "C2",
-            "D2": "D2",
-            "E2": "E2",
-            "F2": "F2",
-            "G2": "G2",
-            "H2": "H2",
-            "I2": "I2",
-            "J2": "J2",
-            "K2": "K2",
-            "L2": "L2",
-            "A1": "A1",
-            "B1": "B1",
-            "C1": "C1",
-            "D1": "D1",
-            "E1": "E1",
-            "F1": "F1",
-            "G1": "G1",
-            "H1": "H1",
-            "I1": "I1",
-            "J1": "J1",
-            "K1": "K1",
-            "L1": "L1",
-        }
-    
-        increment = 1
-        for y in self.dictionary_poc:
-            print(self.dictionary_poc[y], end='|')
-            if increment % 12 == 0:
-                print('')
-            increment += 1
-    
-        print("")
+        # self.dictionary_poc = {
+        #     "A10": "A!",
+        #     "B10": "B!",
+        #     "C10": "C!",
+        #     "D10": "D!",
+        #     "E10": "E!",
+        #     "F10": "F!",
+        #     "G10": "G!",
+        #     "H10": "H!",
+        #     "I10": "I!",
+        #     "J10": "J!",
+        #     "K10": "K!",
+        #     "L10": "L!",
+        #     "A9": "A9",
+        #     "B9": "B9",
+        #     "C9": "C9",
+        #     "D9": "D9",
+        #     "E9": "E9",
+        #     "F9": "F9",
+        #     "G9": "G9",
+        #     "H9": "H9",
+        #     "I9": "I9",
+        #     "J9": "J9",
+        #     "K9": "K9",
+        #     "L9": "L9",
+        #
+        #     "A8": "A8",
+        #     "B8": "B8",
+        #     "C8": "C8",
+        #     "D8": "D8",
+        #     "E8": "E8",
+        #     "F8": "F8",
+        #     "G8": "G8",
+        #     "H8": "H8",
+        #     "I8": "I8",
+        #     "J8": "J8",
+        #     "K8": "K8",
+        #     "L8": "L8",
+        #     "A7": "A7",
+        #     "B7": "B7",
+        #     "C7": "C7",
+        #     "D7": "D7",
+        #     "E7": "E7",
+        #     "F7": "F7",
+        #     "G7": "G7",
+        #     "H7": "H7",
+        #     "I7": "I7",
+        #     "J7": "J7",
+        #     "K7": "K7",
+        #     "L7": "L7",
+        #
+        #     "A6": "A6",
+        #     "B6": "B6",
+        #     "C6": "C6",
+        #     "D6": "D6",
+        #     "E6": "E6",
+        #     "F6": "F6",
+        #     "G6": "G6",
+        #     "H6": "H6",
+        #     "I6": "I6",
+        #     "J6": "J6",
+        #     "K6": "K6",
+        #     "L6": "L6",
+        #     "A5": "A5",
+        #     "B5": "B5",
+        #     "C5": "C5",
+        #     "D5": "D5",
+        #     "E5": "E5",
+        #     "F5": "F5",
+        #     "G5": "G5",
+        #     "H5": "H5",
+        #     "I5": "I5",
+        #     "J5": "J5",
+        #     "K5": "K5",
+        #     "L5": "L5",
+        #
+        #     "A4": "A4",
+        #     "B4": "B4",
+        #     "C4": "C4",
+        #     "D4": "D4",
+        #     "E4": "E4",
+        #     "F4": "F4",
+        #     "G4": "G4",
+        #     "H4": "H4",
+        #     "I4": "I4",
+        #     "J4": "J4",
+        #     "K4": "K4",
+        #     "L4": "L4",
+        #     "A3": "A3",
+        #     "B3": "B3",
+        #     "C3": "C3",
+        #     "D3": "D3",
+        #     "E3": "E3",
+        #     "F3": "F3",
+        #     "G3": "G3",
+        #     "H3": "H3",
+        #     "I3": "I3",
+        #     "J3": "J3",
+        #     "K3": "K3",
+        #     "L3": "L3",
+        #
+        #     "A2": "A2",
+        #     "B2": "B2",
+        #     "C2": "C2",
+        #     "D2": "D2",
+        #     "E2": "E2",
+        #     "F2": "F2",
+        #     "G2": "G2",
+        #     "H2": "H2",
+        #     "I2": "I2",
+        #     "J2": "J2",
+        #     "K2": "K2",
+        #     "L2": "L2",
+        #     "A1": "A1",
+        #     "B1": "B1",
+        #     "C1": "C1",
+        #     "D1": "D1",
+        #     "E1": "E1",
+        #     "F1": "F1",
+        #     "G1": "G1",
+        #     "H1": "H1",
+        #     "I1": "I1",
+        #     "J1": "J1",
+        #     "K1": "K1",
+        #     "L1": "L1",
+        # }
+        #
+        # increment = 1
+        # for y in self.dictionary_poc:
+        #     print(self.dictionary_poc[y], end='|')
+        #     if increment % 12 == 0:
+        #         print('')
+        #     increment += 1
+        #
+        # print("")
     
         # final board
         # adding the row labels on each row will make tile checking more complex - we should discuss
         # if having labelled rows/columns is something we want to have
+
         self.board = {
-            "A10": "10|_",
+            "R10": "10",
+            "A10": "_",
             "B10": "_",
             "C10": "_",
             "D10": "_",
@@ -214,8 +217,9 @@ class BoardClass:
             "J10": "_",
             "K10": "_",
             "L10": "_",
-            
-            "A9": "9 |_",
+
+            "R9": " 9",
+            "A9": "_",
             "B9": "_",
             "C9": "_",
             "D9": "_",
@@ -227,8 +231,9 @@ class BoardClass:
             "J9": "_",
             "K9": "_",
             "L9": "_",
-    
-            "A8": "8 |_",
+
+            "R8": " 8",
+            "A8": "_",
             "B8": "_",
             "C8": "_",
             "D8": "_",
@@ -240,8 +245,9 @@ class BoardClass:
             "J8": "_",
             "K8": "_",
             "L8": "_",
-            
-            "A7": "7 |_",
+
+            "R7": " 7",
+            "A7": "_",
             "B7": "_",
             "C7": "_",
             "D7": "_",
@@ -253,8 +259,9 @@ class BoardClass:
             "J7": "_",
             "K7": "_",
             "L7": "_",
-    
-            "A6": "6 |_",
+
+            "R6": " 6",
+            "A6": "_",
             "B6": "_",
             "C6": "_",
             "D6": "_",
@@ -266,7 +273,8 @@ class BoardClass:
             "J6": "_",
             "K6": "_",
             "L6": "_",
-            "A5": "5 |_",
+            "R5": " 5",
+            "A5": "_",
             "B5": "_",
             "C5": "_",
             "D5": "_",
@@ -278,8 +286,9 @@ class BoardClass:
             "J5": "_",
             "K5": "_",
             "L5": "_",
-    
-            "A4": "4 |_",
+
+            "R4": " 4",
+            "A4": "_",
             "B4": "_",
             "C4": "_",
             "D4": "_",
@@ -291,7 +300,8 @@ class BoardClass:
             "J4": "_",
             "K4": "_",
             "L4": "_",
-            "A3": "3 |_",
+            "R3": " 3",
+            "A3": "_",
             "B3": "_",
             "C3": "_",
             "D3": "_",
@@ -303,8 +313,9 @@ class BoardClass:
             "J3": "_",
             "K3": "_",
             "L3": "_",
-    
-            "A2": "2 |_",
+
+            "R2": " 2",
+            "A2": "_",
             "B2": "_",
             "C2": "_",
             "D2": "_",
@@ -316,8 +327,9 @@ class BoardClass:
             "J2": "_",
             "K2": "_",
             "L2": "_",
-            
-            "A1": "1 |_",
+
+            "R1": " 1",
+            "A1": "_",
             "B1": "_",
             "C1": "_",
             "D1": "_",
@@ -329,25 +341,27 @@ class BoardClass:
             "J1": "_",
             "K1": "_",
             "L1": "_",
-    
-            "A0": "   A",
-            "B0": "B",
-            "C0": "C",
-            "D0": "D",
-            "E0": "E",
-            "F0": "F",
-            "G0": "G",
-            "H0": "H",
-            "I0": "I",
-            "J0": "J",
-            "K0": "K",
-            "L0": "L",
+
+            "R0": "  ",
+            "CA": "A",
+            "CB": "B",
+            "CC": "C",
+            "CD": "D",
+            "CE": "E",
+            "CF": "F",
+            "CG": "G",
+            "CH": "H",
+            "CI": "I",
+            "CJ": "J",
+            "CK": "K",
+            "CL": "L",
         }
-    
+
+    def print_board(self):
         increment = 1
         for y in self.board:
             print(self.board[y], end='|')
-            if increment % 12 == 0:
+            if increment % 13 == 0:
                 print('')
             increment += 1
     
@@ -359,11 +373,12 @@ class BoardClass:
         #Get user input stream and add coodrinates onto board
         #Has to alternated between X and O
         print("It's gonna be fun")
+        # changed the board so that you no longer need to check for tiles with "1 |"
 #        if coordinate is "A1" or "A2" or "A3" or "A4" or "A5" or "A6" or "A7" or "A8" or "A9" or "A10":
 #            self.board[coordinate] = "1 |" + token
 #        else:
 #            self.board[coordinate] = token
         self.board["B2"] = "X"
-        
+
 
 run_Game_Main_Function()
